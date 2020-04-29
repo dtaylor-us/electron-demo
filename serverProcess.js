4ed4fba4b3f360e4ee29c0548275e400f8443097
dtaylor-us <derektaylor.us@gmail.com>
2020-04-29 09:01:25 -0500

    wip

--- serverProcess.js
+++ serverProcess.js
@@ -3,14 +3,31 @@ const childProcess = require('child_process');
 
 const JAVA_PATH = 'java';
 const WIN_PLATFORM = 'win32';
-const JAR_FILE = __dirname +
-  '/dist/rest-service/rest-service-0.0.1-SNAPSHOT.jar';
-const APP_URL = 'http://localhost:8080/greeting';
+// const JAR = __dirname + '/dist/rest-service/rest-service-0.0.1-SNAPSHOT.jar';
+const JAR_PATH = `/dist${process.env.SPRANGTRON_JAR}`;
+const JAR = __dirname + JAR_PATH;
+const APP_URL = process.env.SPRANGTRON_REST_SERVICE_URL;
 // const APP_URL = process.env.APP_URL;
+const fs = require('fs');
+const path = require('path');
+
+function checkJar() {
+  const files = fs.readdirSync(app.getAppPath() + '/dist/rest-service');
+  let filename = null;
+  for (const i in files) {
+    if (path.extname(files[i]) === '.jar') {
+      filename = path.basename(files[i]);
+      break;
+    }
+  }
+  if (!filename) {
+    throw new Error('The Application could not be started');
+  }
+}
 
 function getServerProcess() {
   console.log('::getting jar-path::'); // DEBUG LOG
-  return childProcess.spawn('java', ['-jar', JAR_FILE]);
+  return childProcess.spawn('java', ['-jar', JAR]);
 }
 
 function pingServer(serverProcess, callback, maxAttempts) {
@@ -19,12 +36,12 @@ function pingServer(serverProcess, callback, maxAttempts) {
   function onRejected() {
     return _ => {
       console.log(`ATTEMPTS LEFT::: ${maxAttempts}`);
-      if(maxAttempts > 0){
+      if (maxAttempts > 0) {
         setTimeout(function() {
           pingServer(serverProcess, callback, maxAttempts - 1);
         }, 1000);
       } else {
-        throw new Error('Unable to connect to server.')
+        throw new Error('Unable to connect to server.');
       }
     };
   }
@@ -36,24 +53,25 @@ function pingServer(serverProcess, callback, maxAttempts) {
     };
   }
 
-  requestPromise.get(APP_URL)
-  .then(onFulfilled(callback), onRejected(maxAttempts))
-  .catch((err) => {
-    console.error(err);
-    serverProcess.kill();
-    app.quit()
-  });
+  requestPromise.get(APP_URL).
+    then(onFulfilled(callback), onRejected(maxAttempts)).
+    catch((err) => {
+      console.error(err);
+      serverProcess.kill();
+      app.quit();
+    });
 }
 
 module.exports = class ServerProcess {
   #serverProcess = getServerProcess();
-  constructor() {}
+  constructor() {
+  }
 
-  kill(){
+  kill() {
     if (this.#serverProcess) {
       // kill Java executable
       console.log('killing server');
-     this.#serverProcess.kill('SIGINT');
+      this.#serverProcess.kill('SIGINT');
     }
   }
 
@@ -63,7 +81,7 @@ module.exports = class ServerProcess {
       console.log('Server: ' + data);
     });
     this.#serverProcess.stderr.on('data', (data) => {
-      console.error(`child stderr:\n${data}`);
+      console.error(`${data}`);
       app.quit();
     });
     console.log('Server PID: ' + this.#serverProcess.pid);
